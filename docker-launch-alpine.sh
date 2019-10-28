150dc8bf06f546ad520c5a2c7203b88a5f0cbcd5
Sanoob Pattanath <s.pattanath@searchmetrics.com>
2019-10-28 11:44:26 +0100

    feat: add lauch script with debug

--- docker-launch-alpine.sh
+++ docker-launch-alpine.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-docker run -e "DEBUG=*" --rm -it \
-  --entrypoint='' \
-  -v ~/.aws:/root/.aws \
-  -v ${PWD}:/aws-azure-login \
-  aws-azure-login:alpine "$@" \
-  sh
+if [ $1 -eq 'debug' ]; then
+  docker run -e "DEBUG=*" --rm -it \
+    --entrypoint='' \
+    -v ~/.aws:/root/.aws \
+    -v ${PWD}:/aws-azure-login \
+    aws-azure-login:alpine "$@" \
+    sh
+else
+ docker run --rm -it \
+    -v ~/.aws:/root/.aws \
+    -v ${PWD}:/aws-azure-login \
+    aws-azure-login:alpine "$@"
+fi
