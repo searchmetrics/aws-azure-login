690ba0636014bd9ae13ab4781f699f378de98a62
Maycon-Santos-sm <m.santos@searchmetrics.com>
2020-04-06 13:56:27 +0200

    use our docker

--- docker-launch.sh
+++ docker-launch.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -v ~/.aws:/root/.aws dtjohnson/aws-azure-login "$@"
+_version="latest";
+image_name="searchmetrics/aws-azure-login";
+
+if [ "${1}" = 'debug' ]; then
+  shift
+  docker run -e "DEBUG=aws-azure-login" --rm -it \
+    -v ~/.aws:/root/.aws \
+    -w /aws-azure-login \
+    -v ${PWD}:/aws-azure-login \
+    ${image_name}:${_version} "$@"
+    #sh
+elif [ "${1}" = 'latest' ]; then
+ docker run --rm -it \
+    -v ~/.aws:/root/.aws \
+    ${image_name}:latest "$@"
+else
+ docker run --rm -it \
+    -v ~/.aws:/root/.aws \
+    ${image_name}:${_version} "$@"
+fi
