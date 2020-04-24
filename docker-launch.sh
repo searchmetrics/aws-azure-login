cd83b4ce47d0831e3e3c62f91a2a8038d1c34506
Maycon-Santos-sm <m.santos@searchmetrics.com>
2020-04-24 13:39:12 +0200

    use user id to run the container

--- docker-launch.sh
+++ docker-launch.sh
@@ -6,17 +6,20 @@ image_name="searchmetrics/aws-azure-login";
 if [ "${1}" = 'debug' ]; then
   shift
   docker run -e "DEBUG=aws-azure-login" --rm -it \
-    -v ~/.aws:/root/.aws \
+    -v ~/.aws:/.aws \
     -w /aws-azure-login \
     -v ${PWD}:/aws-azure-login \
+    --user $(id -u) \
     ${image_name}:${_version} "$@"
     #sh
 elif [ "${1}" = 'latest' ]; then
  docker run --rm -it \
-    -v ~/.aws:/root/.aws \
+    -v ~/.aws:/.aws \
+    --user $(id -u) \
     ${image_name}:latest "$@"
 else
  docker run --rm -it \
-    -v ~/.aws:/root/.aws \
+    -v ~/.aws:/.aws \
+    --user $(id -u) \
     ${image_name}:${_version} "$@"
 fi
