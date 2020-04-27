d614833be61034b06ec77a10cc65a4c4178ee33b
Maycon-Santos-sm <m.santos@searchmetrics.com>
2020-04-27 14:36:14 +0200

    use USER env

--- docker-launch.sh
+++ docker-launch.sh
@@ -6,23 +6,23 @@ image_name="searchmetrics/aws-azure-login";
 if [ "${1}" = 'debug' ]; then
   shift
   docker run -e "DEBUG=aws-azure-login" --rm -it \
-    -v ~/.aws:/.aws \
-    -e "HOME=/"
+    -v ~/.aws:/home/$USER/.aws \
+    -e "HOME=/home/$USER" \
     -w /aws-azure-login \
     -v ${PWD}:/aws-azure-login \
-    --user $(id -u) \
+    --user $USER \
     ${image_name}:${_version} "$@"
     #sh
 elif [ "${1}" = 'latest' ]; then
  docker run --rm -it \
-    -v ~/.aws:/.aws \
-    -e "HOME=/"
-    --user $(id -u) \
+    -v ~/.aws:/home/$USER/.aws \
+    -e "HOME=/home/$USER" \
+    --user $USER \
     ${image_name}:latest "$@"
 else
  docker run --rm -it \
-    -v ~/.aws:/.aws \
-    -e "HOME=/"
-    --user $(id -u) \
+    -v ~/.aws:/home/$USER/.aws \
+    -e "HOME=/home/$USER" \
+    --user $USER \
     ${image_name}:${_version} "$@"
 fi
